68a859ef056aae16076d1596fcf5259a3335c500
xeron56 <shahidul1425@cseku.ac.bd>
2024-04-23 10:46:22 +0000

    Update CMake source directory and add platform-specific code in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 // main.cpp
 #include <iostream>
 
+void platformSpecificCode() {
+#ifdef _WIN32
+    std::cout << "Running on Windows" << std::endl;
+#elif __linux__
+    std::cout << "Running on Linux" << std::endl;
+#elif __APPLE__
+    std::cout << "Running on macOS" << std::endl;
+#else
+    std::cout << "Running on an unknown platform" << std::endl;
+#endif
+}
+
 int main() {
-    std::cout << "Hello, CMake!" << std::endl;
+    platformSpecificCode();
     return 0;
 }
